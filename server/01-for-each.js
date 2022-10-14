ed757ed86053c2c4a7ff4b07d15f6c007c58ae58
Mario Castro <mariocastro.pva@gmail.com>
2022-10-14 15:07:27 -0400

    playground map

--- server/01-for-each.js
+++ server/01-for-each.js
@@ -17,3 +17,11 @@ for (const letter in letters) {
         console.log('forin:', element);
     }
 }
+
+const array = [2, 4, 5, 6];
+
+function solution(array) {
+    return array.map((item) => item*2);
+}
+
+console.log(solution(array));
