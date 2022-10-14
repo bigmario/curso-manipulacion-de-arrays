eb6d43c4fd1cce9c50f55e4b882d1c203d6ac34c
Mario Castro <mariocastro.pva@gmail.com>
2022-10-14 16:47:12 -0400

    reduce reloaded

--- server/04-reduce.js
+++ server/04-reduce.js
@@ -2,3 +2,15 @@ const numbers = [5,4,8,6,2]
 
 const reduced = numbers.reduce((sum, item)=>sum+item, 0);
 console.log(reduced);
+
+
+const items = [5, 6, 7, 6, 5, 7, 7, 8]
+const reducedItems = items.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] =1
+    } else {
+        obj[item] = obj[item] +1
+    }
+    return obj
+}, {});
+console.log(reducedItems);
