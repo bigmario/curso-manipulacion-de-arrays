560188cc8c6aa091ff48fc581e25f9c66225a1c7
Mario Castro <mariocastro.pva@gmail.com>
2022-10-17 15:07:07 -0400

    join & split

---
+++ server/09-join.js
@@ -0,0 +1,7 @@
+const elements = ['Fire', 'Air', 'Water'];
+const join = elements.join('--')
+console.log(join);
+
+const curso = 'Curso de Manipulación de Arrays'
+const split = curso.split(' ').join('-').toLowerCase()
+console.log(split);
