66f307adba5810aace666721e2e7a8b3f3ca2d3a
Mario Castro <mariocastro.pva@gmail.com>
2022-10-17 14:26:48 -0400

    every

---
+++ server/06-every.js
@@ -0,0 +1,22 @@
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 18,
+    },
+  ];
+
+  const every = team.every((item) => item.age<=15)
+
+  console.log(every);
